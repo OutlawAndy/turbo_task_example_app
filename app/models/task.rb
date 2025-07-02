bff9f459a920543cc90dc79d48dc06f337bc1fd2
Andy Cohen <outlawandy@gmail.com>
2025-07-01 21:03:00 -0500

    Add ActiveRecord Scopes to Task model.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,8 @@ class Task < ApplicationRecord
   validates :name, presence: true
 
   scope :todo, -> { where(done_at: nil) }
-  scope :done, -> { !todo }
+  scope :overdue, -> { todo.where(due_at: (..Time.current)) }
+  scope :done, -> { where.not(done_at: nil) }
 
   def done=(form_value)
     self.done_at = ActiveRecord::Type::Boolean.new.cast(form_value) ? Time.current : nil
